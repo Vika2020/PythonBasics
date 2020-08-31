8675deb8e27ebe19ea3678518db26c6eb45a49f3
Vika <vika812003@gmail.com>
2020-08-31 19:09:28 -0400

    adding loops

--- lists_intro.py
+++ lists_intro.py
@@ -1,61 +1,156 @@
+# IDE - integrated Development Environment
+# java - eclipse, IntellijIdea
+# python - VScode, pycharm, VIM
+#   CHAPTER 3 : INTRODUCTION TO LISTS
+# print("Hello World!!")
+#
+# # todo: introduction to list data structure
+# # todo: creating a list
+students = ['john', 'mark', 'aziz', 'feruza', 105]
+# # varname assign [0, 1,        2,       3   , 4 ]
+# print(students)
+# print(students[2])
 
+# todo: accessing the elements in the list
+# lists hold values by index, index start with 0
+# print(students[0])  # prints the 1st element in the list
+# print(students[3])  # 'feruza' to be printed
 
-cars = ['BMW', 'Mercedes', 'Lexus']
-print(f"I drive {cars[2]} and I like it.")
-print("I drive " + cars[2] + "and I like it.")
+# reformat the file based on PEP8 rules >> CTRL + ALT + L
 
-cars[0] = 'tesla'
-print(f"after: {cars}")
+# Data structures: lists, tuples, dictionaries (HashMap, Hashset..)
+# create, access the elements, modify, remove element from Data structure, organize
+print(f"Hello, {students[1].title()}! Thank you for coming.")
+# print(f"Hello, {students[4]}! Thank you for coming.")
+# print("Hello, " + str(students[4]) + "! Thank you for coming.")
 
-cars.append('BMW')
-cars.insert(2, 'toyota')
-print(f"after 'modification': {cars}")
+# TIY 3-3. create cars list, print different message for each car
 
-del cars[2]
+# cars = ['bmw', 'nissan', 'mazda', 'honda']
+# print(f"I would travel on {cars[0]}")
+#
+# cars = ['Honda', 'Volvo', 'BMW']
+# print(f"Hello Mark! would you like to drive {cars[0]} or {cars[1]} or {cars[2]}")
+# car = ['honda', 'toyota', 'ford']
+# print(f"I would like to own a " + str(car[2]) + " car")
+
+# MODIFY LISTS
+cars = ['bmw', 'nissan', 'mazda', 'honda']
+print(f"before: {cars}")
+cars[2] = 'tesla'  # this will replace 'mazda'
+print(f'after "modification": {cars}')
+
+# ADDING ELEMENT
+# appending
+cars.append('lexus')  # this will add the 'lexus' to the end of the list
 print(cars)
 
-cars.remove('tesla')
+# inserting
+cars.insert(2, 'toyota')  # this will insert and move all other object to right, nothing lost
 print(cars)
 
-cars.pop()
-print(cars)
+# REMOVE FROM THE LIST
+# delete by index
+print(cars[5])
+del cars[4]
+print(f"after del {cars}")
+# print(cars[5]) # you will get >> IndexError: list index out of range
+#  comment the line where  your cursor is >> CTRL + /
 
-cars.append('BMW')
-print(cars)
+cars.pop()  # returns the value that is being removed
+print(f"after pop: {cars}")
 
-guests = ['Family', 'Friends', 'Coworkers']
-print(f"I invite guests  but {guests[1]} could not make it.")
-guests[1] = 'classmates'
-print(guests)
+cars.pop(2)
+print(f"after pop(2): {cars}")
 
-sorted_guests = sorted(guests)
+# delete by value
+cars.remove('nissan')
+print(f"after remove :{cars}")
+
+# # TIY 3-4 and 3-5
+# print(f"welcome to the dinner {guests[0]}"). Guest List: If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+guests = ['D.Trump', 'Putin', 'Messi', 'CRonaldo', 'Alex Morgan']
+print("================= 3-4 =========================")
+print(f"welcome to the dinner {guests[0]}!")
+print(f"welcome to the dinner {guests[1]}!")
+print(f"welcome to the dinner {guests[2]}!")
+print(f"welcome to the dinner {guests[3]}!")
+print(f"welcome to the dinner {guests[4]}!")
+
+# 3-5. Changing Guest List: You just heard that one of your guests can’t make the
+# dinner, so you need to send out a new set of invitations. You’ll have to think of
+# someone else to invite.
+# • Start with your program from Exercise 3-4. Add a print statement at the
+# end of your program stating the name of the guest who can’t make it.
+# • Modify your list, replacing the name of the guest who can’t make it with
+# the name of the new person you are inviting.
+# • Print a second set of invitation messages, one for each person who is still
+# in your list.
+# 'M.Sharapova'
+guests_not_coming = []
+guests_not_coming.append(guests[0])
+guests[0] = 'M.Sharapova'
+
+print("================= 3-5 =========================")
+print(f"{guests[4]} sorry to hear that, please come next time.")
+print(f"Guests are coming: {guests}")
+print(f"Guests are NOT coming: {guests_not_coming}")
+print(f"welcome to the dinner {guests[0]}!")
+print(f"welcome to the dinner {guests[1]}!")
+print(f"welcome to the dinner {guests[2]}!")
+print(f"welcome to the dinner {guests[3]}!")
+print(f"welcome to the dinner {guests[4]}!")
+
+# Organizing your list
+# temporary and permanent sorting
+print(sorted(guests))  # temp sorting , sorted() works for some other data structures
+sorted_guests = sorted(guests)  # sorted() - returns you the copy of the list but sorted in asc
 print(sorted_guests)
 print(guests)
 
-guests.sort()
-print(guests)
 guests.reverse()
-print(guests)
+print(f"Reversing the list: {guests}")
 
+guests.sort()  # only works for list and does not return anything, just effects the orig list
+print(f"Perm sorting with list.sort() : {guests}")
+guests.sort(reverse=True)
+print(f"Perm reverse (desc) sorting with list.sort() : {guests}")
 
-nums = [2, 35, 8, 21, 7]
-nums.pop(3)
+# list.reverse() - reverses but not desc order
+# list.sort(reverse=True) - sorts in desc order
+print()
+nums = [4, -10, 9, 5, 6, 1, 0, 44]
+print(nums)
+nums.reverse()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+# nums[8] = 100 # IndexError: list assignment index out of range
+nums.insert(8, 100)
 print(nums)
 
-nums = [2, 35, 8, 21, 7]
-nums.remove(7)
-print(nums)
-print(len(nums))
+print(f"Number of elements in my 'nums' list :{len(nums)}")
+num_elements = len(nums)
+print(nums[-1])
+print(nums[-5])
+print(nums[-10]) # IndexError: list index out of range
 
-# class
+# All you need to know about the LIST
+# list_name = [] - creating an empty list
+# list.append(newValue)
+# list.insert(ind, value)
+# del list[ind]
+# list.remove(value)
+# list.pop() - removes and returns the last element, list.pop(ind)
+# list[ind] = value  - assigning a new value to existing Index
+# list.sort(), list.sort(reverse=True)
+# sorted(list) -  copies the list and returns sorted copy of the list
+# list.reverse()
+# len(list) - returns the length of your list (how big is list, i.e. number of elements)
+# list[-n] - index start from the end of the list, last element is list[-1]
 
-
-
-
-
-
-
-
-
-
-
+# HW: 3-6, 3-7
+# HW TIY 3-8
--- lists_loops.py
+++ lists_loops.py
@@ -1,387 +1,154 @@
-# # # # Chapter 4: Working with LIST
-# # #
-# # # states = ['New York', 'New Jersey', 'Connecticut', 'Virginia', 'California']
-# # #
-# # # # for variable in list_of_elements:
-# # # #     repetitive code here
-# # #
-# # # for state in states:
-# # #     print(f"Welcome to {state}!!")
-# # #
-# # #     print(f"Enjoy your trip {state.upper()}!!")
-# # #
-# # # # example
-# # #
-# # #
-# # # pizzas = {'four cheese', 'vegetarian', 'pepperoni'}
-# # # for pizza in pizzas:
-# # #     print(f"I am going order {pizza} for dinner!")
-# # # print('What can be batter than pizza?!')
-# #
-# #  # working with numbers
-# # for num in range(5):
-# #     print(f"My current number from range(5): {num}")
-# # for num in range(3, 6):
-# #     print(f"My current number from range(3,6): {num}")
+# Chapter 4: Working with LISTS
+states = ['New York', 'New Jersey', 'Connecticut', 'California']
+# Loops - within the loops you create repetitive actions
+
+# for variable in list_of_elements:
+#     repetitive code here
+
+# for state in states:
+#     print(f"Welcome to {state}!!")
+#     print(f"Welcome to {state}!!")
+# pass # do nothing
+
+# Things to Remember while working with LOOPS
+#  colon at the end of 'for' line
+#  'in' in the 'for' line
+#  give any name to a variable and use that variable inside the for loop code
+#  lines of code that belongs to for loop (repetitive code) must be indented
+#  Always check your indentation
+
+# states >> 4 members >>
+#     >> 1st loop/round >> state = 'New York'   -- python does this
+#     >> print(f"Welcome to {state}!!")         -- we will write this code
+#     >>> Welcome to New York!!                 -- this is execution
+
+#     >> 2nd loop/round >> state = 'New Jersey'
+#     >> print(f"Welcome to {state}!!")
+#     >>> Welcome to New Jersey!!
+
+#     >> 2nd loop/round >> state = 'Connecticut'
+#     >> print(f"Welcome to {state}!!")  --- this code we will write
+#     >>> Welcome to Connecticut!!
+
+# print(states)
+# # Pycharm: refactoring >> Shift + f6
 #
-# numbers = list(range(5))
-# print(numbers)
+# # print(state)  # outside of the scope of variable
+# # SCOPE
+# for state in states:
+#     print(f"Welcome to {state.upper()}!!")
 #
-# # print squares of numbers from 5 to 12
-# for num in range(5,13):
-#     print(num**2)
+# print(f"Enjoy your trip!!") # outside of loop, independent from loop
+# print(state)  # Incorrect : outside of the scope of variable
+# print()
+# # TIY 4-1 Pizza exercise
+# pizzas = ['pepperoni', 'four cheese', 'salami', 'mashrooms']
+# for pizza in pizzas:
+#     print(f"I really love {pizza}!")
 #
-# squares = []
-# for num in range(5, 13):
-#     num_sqr = num ** 2
-#     squares.append(num_sqr)
-#     print(num_sqr)
-#     print(squares)
-#
-# print(squares)
-
-# numbers = [2, 54, 76, 3, 7, 56, 2]
-# print(f"Min number from numbers : {min(numbers)}")
-# print(f"Max number from numbers : {max(numbers)}")
-# print(f"Sum number from numbers : {sum(numbers)}")
-
-# name = "John Doe"
-# print(name[5:])
-#
-# numbers = [2, 54, 76, 3, 7, 56, 2]
-# print(numbers[3:])
-# print(numbers[2:-2])
-
-# numbers = [2, 54, 76, 3, 7, 56, 2]
-# numbers_copy = numbers[:]
-# print(numbers_copy)
-#
-# numbers_copy
-
-# Ex. 4-10
-
-# numbers = [2, 54, 76, 3, 7, 56, 2]
-# print("The first three numbers in the list are:")
-# for number in numbers[:3]:
-#     print(number)
-
-# my_pizza = ['four cheese', 'vegetarian', 'pepperoni']
-# friends_pizza = my_pizza[:]
-# print(my_pizza)
-# print(friends_pizza)
-# my_pizza.append('margarita')
-# friends_pizza.append('artichoke')
-# print(my_pizza)
-# print(friends_pizza)
-
-
-#
-# foods = ('salad', 'meat', 'bread', 'dessert', 'drinks')
-# for food in foods:
-#     print(food.title())
-
-
-# name = 'Victoriya'
-# # for a in name:
-# #     print(a)
-#
-#
-# friends = ['vika', 'olya', 'saga', 'saida']
-# print(friends[-1])
-#
-# for i in friends:
-#     print(i)
-#
-# for i in range(len(friends)):
-#     print(f"element on index {i} is {friends[i]}")
-
-# pizzas = ['margarita', 'four cheese', 'vegetarian', 'pepperoni']
-# print(pizzas)
-# print(sorted(pizzas))
-# print(sorted(pizzas, reverse=True))
-# print(pizzas)
-#
-# pizzas.reverse()
-# print(pizzas)
-#
-# pizzas.sort(reverse=True)
-# print(pizzas)
-#
-# pizzas.count()
-# print(pizzas)
-
-#
-# pizzas = ['four cheese', 'vegetarian', 'pepperoni']
-# pizzas.pop(0)
-# print(pizzas)
-# pizzas.insert(2,'margarita')
-# print(pizzas)
-# pizzas.append('pineapple')
-# print(pizzas)
-# pizzas.remove('pineapple')
-# print(pizzas)
-# len(pizzas)
-# print(pizzas)
-
-# student1 = {'name': 'Vika', 'gpa': 4.0}
-# student2 = {'name': 'Saida', 'gpa': 3.8}
-#
-# print(student1)
-# print(student1['name'], student1['gpa'])
-
-
-# num_1 = 18
-# num_2 = 21
-# num_1 > 18 and num_2 < 21
-
-
-# list_of_students = ['Vika', 'Maxi', 'Steve', 'Sara']
-# 'Vika' not in list_of_students
-#
-# 'Akobir' in list_of_students
-#
-# banned_users = ['Maria', 'Karl', 'Evan']
-# if 'Karl' not in banned_users:
-#     print('You can leave a comment')
-# else:
-#     print('You are not allowed to leave a comment')
-
-# banned_users = ['Maria', 'Karl', 'Evan']
-# user_1 = 'Evan'
-# user_2 = 'Sara'
-#
-# if user_1 not in banned_users:
-#     print(user_1.upper() + " , you can leave a comment." )
-# else:
-#     print(user_2.upper() + ", you are not allowed to write a comment!")
-
-
-# Ex 5-1,2
-
-# city = 'Paris'
-# print("Is city == 'Paris'? I predict True.")
-# print(city == 'Paris')
-#
-# print("Is city != 'Paris'? I predict False")
-# print(city == 'Miami')
-
-# age = 17
-# if age >= 18:
-#     print("You can vote!")
-#     print("Have you registered to vote yet?")
-# else:
-#     print('Sorry, you cannot vote!')
-#
-# age = 66
-# if age <4:
-#     cost = 0
-# elif age < 12:
-#     cost = 5
-# elif age >= 65:
-#     cost = 8
-# else:
-#     cost = 12
-# print("Your admission cost is $" + str(cost) + ".")
-
-# =======  Fuzz - Buzz problem =======
-
-# for i in range(3):
-#     num = (int(input("Enter your number:")))
-#     if num % 3 == 0 and num % 5 ==0:
-#         print('FuzzBuzz')
-#     elif num % 5 ==0:
-#         print('Buzz')
-#     elif num % 3 ==0:
-#         print('Fuzz')
-#
-# rivers = {'nile': 'egypt', 'hudson': 'usa', 'volga': 'russia', 'mississippi': 'usa', 'thames': 'uk'}
-#
-# for river, country in rivers.items():
-#     # if country == 'usa' or country == 'uk':
-#     if country in ['usa', 'uk']:
-#        print(f" The {river.upper()} runs through {country.upper()}")
-#     else:
-#        print(f"The {river.title()} runs through {country.title()}")
-#
-# print('Rivers are: ')
-# for river in rivers.keys():
-#         print('\t', river.title())
-# print('Countries are:')
-# for country in rivers.values():
-#     if country in ['usa', 'uk']:
-#         print('\t', country.upper())
-#     else:
-#         print('\t', country.title())
-
-#
-# cities = {'New York': {'country': 'USA', 'population': 8.4, 'fact': 'Big Apple'},
-#            'Istanbul': {'country': 'Turkey', 'population': 15.52 , 'fact': 'Constatinople'},
-#            'Moscow': {'country': 'Russia', 'population': 12.53, 'fact': 'Red Square'}}
-#
-# for city, info in cities.items():
-#     # print(city)
-#     # print(info)
-#     print(f"{city} is very beautiful city in {info['country']}."
-#           f"It has {info['population']} mln population and the city is also know as {info['fact']}.")
-
- # Ex. 5-3
-
-# alien_color = 'green'
-# if alien_color == 'green':
-#     print('You just earned 5 points')
-# elif alien_color == 'red':
-#     print('You just earned 10 points')
-# else:
-#     print('You just earned 15 points')
-
-
-# person_age = 5
-# if person_age < 2:
-#     print("This person is baby")
-# elif person_age >= 2 and person_age < 4:
-#     print("This person is toddler")
-# elif person_age >= 4 and person_age < 13:
-#     print("This person is kid")
-# elif person_age >= 13  and person_age < 20:
-#     print("This person is a teenager")
-# elif person_age  >= 20 and person_age  < 65:
-#     print("This person is an adult")
-# else:
-#     print("This person is an elder")
-
-# fruits = ['apple', 'orange', 'banana']
-# if 'apple' in fruits:
-#     print('You really like apple')
-# if 'orange' in fruits:
-#     print('You really like orange')
-# if 'banana' in fruits:
-#     print('You really like bananas')
-
-# ==== Dictionaries ====
-
-#  creating dictionary and adding a new value #
-
-# friend_1 = {'name': 'Oksi', 'age': '37', 'address': 'Richmond'}
-# print(friend_1)
-# friend_1['status'] = 'married'
-# print(friend_1)
-# friend_1['children'] = 2
-# print(friend_1)
-# print(friend_1['status'])
-
-# ==== changing value ===
-
-# friend_2 = {}
-# friend_2['name'] = 'Akobir'
-# friend_2['status'] = 'married'
-# friend_2['kids'] = 1
-# print(friend_2)
-# friend_2['kids'] = 2
-# print(friend_2)
-# print("I know " + friend_2['name'] + " for many years. \nHe has " + str(friend_2['kids']) + " kids.")
-
-# ==== delete value ===
-
-# friend_1 = {'name': 'Oksi', 'age': '37', 'address': 'Richmond'}
-# del friend_1['address']
-# print(friend_1)
-# friend_1['kids'] = 2
-# print(friend_1['name'].upper() + " and her husband have " + str(friend_1['kids']) + " kids.")
-
-
-# mama = {'first_name': 'Raisa', 'last_name': 'Dogbaeva', 'age': 71, 'city': 'Brooklyn'}
-# print(mama['first_name'])
-# print(mama['last_name'])
-# print(mama['age'])
-# print(mama['city'])
-
-# fav_numbers = {'Vika': 14, 'mama': 18, 'papa': 16}
-# print(fav_numbers)
-
-# === looping through dictionary ===
-
-
-# full_name = {'name': 'Vika', 'middle name': 'Ageevna', 'last name': 'Dogbaeva'}
-# for key, value in full_name.items():
-#     print("\nKey:" + key)
-#     print("Value:" + value)
-
-
-# fav_numbers = {'Vika': 14, 'mama': 18, 'papa': 16}
-# fav_numbers['bro'] = 17
-# print(fav_numbers)
-
-# for name, number in fav_numbers.items():
-#     print("\nName:" + name)
-#     print("Number:" + str(number))
-
-# for name in fav_numbers:
-#     print(name.title())
-#
-# fav_numbers = {'Vika': 14, 'mama': 18, 'papa': 16}
-# for name in fav_numbers.keys():
-#     print(name.title())
-
-# fav_numbers = {'vika': 14, 'papa': 16, 'mama': 18}
-# print(fav_numbers)
-# # family = ['mama', 'papa']
-# # for name in fav_numbers.keys():
-# #     print(name.upper())
-# #
-# #     if name in family:
-# #        print("\nHello " + name.title() + ". " "\nI see your favorite number is " + str(fav_numbers[name]) + ".")
-# # if 'bro' not in fav_numbers.keys():
-# #     print("Bro, what's your favorite number?")
-
-# sort keys
-# for name in sorted(fav_numbers.keys()):
-#     print(name.title() + " , thank you for sharing your favorite number!")
-
-# fav_numbers = {'Vika': 14, 'mama': 18, 'papa': 16}
-# print("Your favorite numbers are:")
-# for number in fav_numbers.values():
-#     print(number)
-
-# Ex. 6-5
-
-rivers = {'Volga': 'Russia', 'Nile': 'Africa', 'Amazon': 'Africa'}
-# for key, value in rivers.items():
-#     print("The " + (key) + " runs trough " + (value) + ".")
-#
-# for key in rivers:
-#     print(key)
-# for value in rivers.values():
-#     print(value)
-
-# === Putting a dictionary inside a list ===
-
-# apt_1 = {'location': 'Brooklyn', 'population': '20 mln', 'price': 'avr'}
-# apt_2 = {'location': 'The Bronx', 'population': '14 mln', 'price': 'law'}
-# apt_3 = {'location': 'New York', 'population': '12 mln', 'price': 'high'}
-#
-# apartments = [apt_1, apt_2, apt_3]
-# for apt in apartments:
-#     print(apt)
-
-# === Putting a list inside a dictionary ===
-
-apartment = {'building': 'new','amenities': ['gym', 'pool','rooftop'],}
-print("I have an apartment in a " + apartment['building'] + " building with:")
-for amenity in apartment['amenities']:
-    print("\t" + amenity)
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print()
+# pizzas = ["cheese", "artichoke", "mashroom"]
+# for pizza in pizzas:
+#     print(pizza)
+#     print(f"I like this type of pizza {pizza}!!!")
+# print("I really love pizza")
+
+# HW 4-2
+
+# Marking Numerical List
+for num in range(5):  # from 0 to 4
+    print(f"My current number from range(5): {num}")
+
+for num in range(3, 6):  # from 3 to 5
+    print(f"My current number from range(3, 6): {num}")
+
+# list(object) - mutable >> [4, 6, 12]
+# tuple -   (4, 6, 12) - immutable
+friends = list()
+students = []
+
+nums = list(range(5))  # 1. range() >> 0 1 2 3     2. list() >> [0,1,2,3,4]     3. numbers = [0,1,2,3,4]
+print(nums)
+
+# print squares of numbers from 5 to 12
+squares = []  # squares = list()
+for num in range(5, 13):
+    num_sqr = num ** 2
+    # squares.insert(-1, num_sqr)
+    squares.append(num_sqr)
+    # print(num_sqr)
+    # print(squares)
+print(squares)
+
+squares = list()  # resetting the list to empty list
+for num in range(5, 13):
+    squares.append(num ** 2)
+print(squares)
+
+nums = [5, 78, 456, 127, 230, 6, 5]
+# min(list) , max(list), sum(list)
+print(f"Min number from numbers :{min(nums)}")
+print(f"Max number from numbers :{max(nums)}")
+print(f"Sum number from numbers :{sum(nums)}")
+
+# list comprehensions
+
+squares = []
+for num in range(5, 14, 2):
+    squares.append(num ** 2)
+
+# FYI
+# squares = list(num ** 2 for num in range(5, 14))
+squares = [num ** 2 for num in range(5, 14, 2)]
+print(squares)
+
+print("# TIY 4-5")
+# for num in range(1, 1000001):
+#     pass
+#     # code to create a list
+# print(min(num))
+# print(max(num))
+# print(sum(num))
+# print(sum(num) / len(num))
+
+nums = list(range(1, 1000001))
+print(f"Min number is: {min(nums)}")
+print(f"Max number is: {max(nums)}")
+print(f"Sum number is: {sum(nums)}")
+
+# Slicing the list
+name = "John Doe"
+print(name[5:])  # >>> Doe
+
+numbers = [5, 78, 456, 127, 230, 6, 5]
+print(numbers[:3])  # indexes >> 0, 1, 2
+print(numbers[0:3])
+
+print(numbers[3:])  # from 3rd index to end of the list
+print(numbers[1:-2])  # from 1st index(second element) to third from the end
+
+new_numbers = numbers  # this is linking the numbers_copy to numbers list
+# if numbers is modified numbers_copy will effected
+numbers_copy = numbers[:]  # copying the list to a new list
+
+print("# copying the lists example")
+print(numbers, new_numbers, numbers_copy)
+numbers.append(5555)
+print(numbers, new_numbers, numbers_copy)
+
+for num in numbers[3:]:
+    print(num)
+
+# HW 4-2
+# HW 4-6, 4-7, 4-8, 4-9
+# HW 4-10, 4-11, 4-12
+
+
+add_numbers = [1, 25, 350, 550, 750, 1000000]
+print(f"Min number from numbers :{min(add_numbers)}")
+print(f"Max number from numbers :{max(add_numbers)}")
+# why when you use (max function) python see last numbers?
+print(f"Sum number from numbers :{sum(add_numbers)}")
 
 
 
